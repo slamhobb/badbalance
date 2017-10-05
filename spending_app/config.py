e351de68120c54ad66e2f9f141b5e1c3e6c1ccb7
GKar <demkon88@mail.ru>
2017-10-05 21:39:51 +0300

    replace db from project

--- spending_app/config.py
+++ spending_app/config.py
@@ -2,7 +2,7 @@ import os
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
-DATA_BASE_CONNECTION_STRING = os.path.join(_basedir, 'bad_balance.db')
+DATA_BASE_CONNECTION_STRING = os.path.join(_basedir, '..', '..', 'badbalance-db', 'bad_balance.db')
 
 AUTH_TOKEN_NAME = 'bb-auth-token'
 
