b6d72720052e866ca038ed93bf11978dd09f1353
GKar <demkon88@mail.ru>
2019-06-23 00:41:30 +0300

    update eslint config

--- frontend/tools/dateTools.js
+++ frontend/tools/dateTools.js
@@ -1,7 +1,7 @@
 'use strict';
 
 export function dateToString(date) {
-    let localDate = new Date(date);
+    const localDate = new Date(date);
     localDate.setMinutes(localDate.getMinutes() - localDate.getTimezoneOffset());
 
     return localDate.toISOString().slice(0, 10);
