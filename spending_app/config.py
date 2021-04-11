8bc4506c4d85eec76cdae26d14d6fd0397f9c5ad
GKar <demkon88@mail.ru>
2021-04-11 18:25:33 +0300

    config from file

--- spending_app/config.py
+++ spending_app/config.py
@@ -1,9 +1,19 @@
-import os
+from os import path
+from base64 import b64decode
+from json import load
 
-_basedir = os.path.abspath(os.path.dirname(__file__))
+_basedir = path.abspath(path.dirname(__file__))
 
-DATA_BASE_CONNECTION_STRING = os.path.join(_basedir, '..', '..', 'badbalance-db', 'bad_balance.db')
+_config_path = path.join(_basedir, '..', '..', 'badbalance-cfg', 'config.json')
 
-AUTH_TOKEN_NAME = 'bb-auth-token'
 
-SECRET_KEY = b'\x14ov\x1f\xaa\x08C]\xf5o\x13\x0bB\x0e\x8ev\xfd#:\xd3\xc7\xec\xcb\xdf'
+def _get_config():
+    with open(_config_path, 'r') as f:
+        return load(f)
+
+
+config = _get_config()
+
+DATA_BASE_CONNECTION_STRING = config['DATA_BASE_PATH']
+AUTH_TOKEN_NAME = config['AUTH_TOKEN_NAME']
+SECRET_KEY = b64decode(config["SECRET_KEY_BASE64"].encode())
